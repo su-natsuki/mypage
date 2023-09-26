e1a30c81b8810fd88d1c1ad2695883b60ce836c0
su-natsuki <137774047+su-natsuki@users.noreply.github.com>
2023-09-26 20:58:54 +0900

    Add files via upload

---
+++ billboard.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://sparta:test@cluster0.o71g4ig.mongodb.net/Cluster0?retryWrites=true&w=majority')
+db = client.dbsparta
+
+URL = "https://www.billboard-japan.com/charts/detail?a=hot100"
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get(URL, headers=headers)
+soup = BeautifulSoup(data.text, 'html.parser')
+
+musics = soup.select('#content2 > div > div.leftBox > table > tbody > tr')
+
+for music in musics:
+    rank = music.select_one('td > span').text
+    title = music.select_one('p.musuc_title').text.strip()
+    artist = music.select_one('p.artist_name').text.strip()
+    print(rank,title,artist)
